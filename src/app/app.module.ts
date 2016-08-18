d40abbafa2412d077c35d447c3ed258affbac965
antonyroberts <antonyrobertsgit@gmail.com>
2016-08-18 00:00:14 -0400

    updating gcloud info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { HttpModule } from '@angular/http'
 
+import { MdButtonModule } from '@angular2-material/button';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +14,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    //materialDesign
+    MdButtonModule
   ],
   providers: [],
   entryComponents: [AppComponent],
